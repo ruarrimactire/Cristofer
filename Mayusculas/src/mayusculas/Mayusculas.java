bdea395d1c7ff7c354ba820946ef91a9f664d745
Risto <risto_1991@hotmail.com>
2014-07-04 11:22:54 +0200

    Proyecto mayusculas. -> Diferencia entre cadenas y arrays, diferencia entre print y println, bucles for y while.

--- Mayusculas/src/mayusculas/Mayusculas.java
+++ Mayusculas/src/mayusculas/Mayusculas.java
@@ -21,10 +21,38 @@ public class Mayusculas {
         System.out.println ("Por favor, introduzca su nombre y apellido.");
         String nombre = usoComun.leerLinea();
         
-        System.out.println(
-                nombre.substring(0, 1).toUpperCase() +
-                        nombre.substring(1).toLowerCase()
-        );
+        String [] letras = new String [20];
+        for (int i = 0; i<20; i++){
+            letras[i] = new String();
+        }
+        /**
+         * En un string no se pueden modificar los elementos, solo añadir mas.
+         * Un array separa la cadena en celdas y se pueden modificar los elementos.
+         */
+        int j = 0;
+        for (int i = 0; i< nombre.length(); i++ ){
+            if (nombre.charAt(i) == ' ' || nombre.charAt(i) == '\t'){
+                continue;
+            }
+            else {
+                while (i< nombre.length() && nombre.charAt(i) != ' ' && 
+                        nombre.charAt(i) != '\t' ){
+                    letras[j] += nombre.charAt(i);
+                    i++;
+                }
+                j++;
+            }
+        }
+        
+        System.out.println("dimension del array letras: " + letras.length);
+        System.out.println("celdas rellenadas del array letras: " + j);
+        
+        for (int i = 0; i < j; i++ ){
+            System.out.print( letras[i].substring(0,1).toUpperCase() +
+                    letras[i].substring(1).toLowerCase() + " " );
+        }
+        System.out.print('\n');
+        
        /**
         * toUpperCase es un metodo de la clase String, que cambia por mayusculas
         *       las letras de las posiciones indicadas.
