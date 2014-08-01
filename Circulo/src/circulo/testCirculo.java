737a751be81aecd485282047fbdabb17bcafdc19
Risto <risto_1991@hotmail.com>
2014-08-01 11:24:25 +0200

    Ejemplo de clase circulo para mostrar el uso de los finalizadores. Como en el capitulo 3, pagina 24.

---
+++ Circulo/src/circulo/testCirculo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package circulo;
+
+/**
+ *
+ * @author Risto
+ */
+public class testCirculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Declaracion de tres objetos Circulo.
+        Circulo c1, c2, c3;
+        // Inicializacion de los tres objetos Circulo.
+        c1 = new Circulo("azul", 15.0);
+        c2 = new Circulo("verde", 15.0);
+        c3 = new Circulo("rojo", 15.0);
+        
+//        System.out.println("Se muestra el area del circulo c1 = " + c1.area() );
+//        System.out.println("Se muestra el area del circulo c2 = " + c2.area() );
+//        System.out.println("Se muestra el area del circulo c3 = " + c3.area() );
+
+        // Destruccion de los tres objetos Circulo.
+        c1 = null;
+        c2 = null;
+        c3 = null;
+        
+        System.out.println("numObjetos = " + Circulo.numObjetos());
+    
+    }
+}
