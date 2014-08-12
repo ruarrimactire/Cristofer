17d21b7b200c3ca05789fa382bfb7215ebd715d1
Risto <risto_1991@hotmail.com>
2014-08-12 11:15:47 +0200

    Proyecto recursividad. Sobreescritura de los metodos toString y equals.

--- recursividad/src/usoComun/numera.java
+++ recursividad/src/usoComun/numera.java
@@ -70,10 +70,8 @@ public class numera {
         }
         else {
             if ( comparacion.equals(referencia.getString(0)) ){
-                System.out.println( referencia.getPosiciones(8) );
-                System.out.println( referencia.getString(8) );
-                System.out.println( original.subtraer(referencia).getPosiciones(5) );
-                System.out.println( original.subtraer(referencia).getString(5) );
+                System.out.println( referencia.toString() );
+                System.out.println( original.subtraer(referencia).toString() );
                 System.out.println( "-----" );
                 return 1;
             }
